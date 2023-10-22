8b83debc849a641505139830395b1c46fe23a5fa
Vikri Permana <vikripermana91@gmail.com>
2023-10-22 17:15:53 +0700

    :sparkles: :lipstick: : Add Fuel Mechanic

--- Codes/LevelManager.gd
+++ Codes/LevelManager.gd
@@ -4,3 +4,10 @@ var coins_collected = 0
 
 func add_coins(amount):
 	coins_collected += amount;
+	$UI/Coin/Label.text = str(coins_collected)
+
+func updateFuelUI(value):
+	$UI/Fuel/ProgressBar.value = value
+	var stylebox = $UI/Fuel/ProgressBar.get_theme_stylebox("fill")
+	stylebox.bg_color.h = lerp(0.0,0.3,value/100)
+	print(stylebox.bg_color)
--- Codes/player.gd
+++ Codes/player.gd
@@ -4,16 +4,33 @@ var wheels = [];
 var speed = 60000;
 var max_speed = 60;
 
+var fuel = 100
+
 func _ready():
 	wheels = get_tree().get_nodes_in_group("wheel")
+	get_parent().updateFuelUI(fuel)
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		for wheel in wheels:
-			if wheel.angular_velocity < max_speed :
-				wheel.apply_torque_impulse(speed * delta * 60);
-	
-	if Input.is_action_pressed("ui_left"):
-		for wheel in wheels:
-			if wheel.angular_velocity > -max_speed :
-				wheel.apply_torque_impulse(-speed * delta * 60);
+	if fuel > 0 :
+		if Input.is_action_pressed("ui_right"):
+			useFuel(delta)
+			for wheel in wheels:
+				if wheel.angular_velocity < max_speed :
+					wheel.apply_torque_impulse(speed * delta * 60);
+		
+		if Input.is_action_pressed("ui_left"):
+			useFuel(delta)		
+			for wheel in wheels:
+				if wheel.angular_velocity > -max_speed :
+					wheel.apply_torque_impulse(-speed * delta * 60);
+				
+	print(fuel)
+
+func refuel():
+	fuel = 100
+	get_parent().updateFuelUI(fuel)
+
+func useFuel(delta):
+	get_parent().updateFuelUI(fuel)
+	fuel -= 10 * delta
+	fuel = clamp(fuel,0,100)
